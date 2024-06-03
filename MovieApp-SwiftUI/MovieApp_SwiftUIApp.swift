ae112c0f25a99861b9b34fc63b9ce22d5615356f
AGorkemAksoz <a.gorkemaksoz@gmail.com>
2024-06-03 14:13:05 +0300

    Initial Commit

---
+++ MovieApp-SwiftUI/MovieApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieApp_SwiftUIApp.swift
+//  MovieApp-SwiftUI
+//
+//  Created by Gorkem on 3.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct MovieApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
